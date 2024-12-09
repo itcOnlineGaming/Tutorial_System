9f869b0ea838cae89d8c08086061b5e71fc996ee
RuslanGavrilov <93270975+RuslanG4@users.noreply.github.com>
2024-12-09 12:28:38 +0000

    Create a button that showcases closing popUps #2
    Added close all pop ups button

--- TutorialPackage/Assets/Scripts/TutorialController.cs
+++ TutorialPackage/Assets/Scripts/TutorialController.cs
@@ -10,12 +10,17 @@ public class TutorialController : MonoBehaviour
     public TMP_FontAsset fontAsset;
     public PopUpManager popUpManager;
 
+    public GameObject closeButtonRef;
+    public GameObject closeOneButtonRef;
+
     public Transform position;
 
     public bool hasShownTutorial;
     // Start is called before the first frame update
     void Start()
     {
+        closeButtonRef.SetActive(false);
+        closeOneButtonRef.SetActive(false);
         //loads possible bool if exists
         hasShownTutorial = BoolSaveSystem.LoadBool("hasShownTutorial");
         //Subscirbe to all events
@@ -28,7 +33,7 @@ public class TutorialController : MonoBehaviour
         TutorialServiceLocator.TutorialEventManager.Subscribe(TutorialEvent.SwipeTutorial, popUpManager.ShowSwipePopUp);
 
         //create your own way of showing the popUps
-        if (!hasShownTutorial)
+        if (hasShownTutorial)
         {
             StartCoroutine(ShowTutorials());
         }
@@ -68,18 +73,77 @@ public class TutorialController : MonoBehaviour
 
         TutorialServiceLocator.TutorialEventManager.RaiseEvent(TutorialEvent.ArrowTutorial, position, ArrowData);
 
-       yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(3f);
 
         TutorialServiceLocator.TutorialEventManager.RaiseEvent(TutorialEvent.ShakeTutorial, position, TextBoxData);
 
-       yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(3f);
 
-       TutorialServiceLocator.TutorialEventManager.RaiseEvent(TutorialEvent.SwipeTutorial, position, SwipeData);
+        TutorialServiceLocator.TutorialEventManager.RaiseEvent(TutorialEvent.SwipeTutorial, position, SwipeData);
 
-       yield return new WaitForSeconds(3f);
-
+        yield return new WaitForSeconds(3.1f);
 
        BoolSaveSystem.SaveBool("hasShownTutorial", true); //save your boolean to show true
+
+       ClosePopUp();
+    }
+
+    public void ClosePopUp()
+    {
+        closeButtonRef.SetActive(true);
+        closeOneButtonRef.SetActive(true);
+        StartCoroutine(CreateNewPopUpToDestroy());
+    }
+
+    public IEnumerator CreateNewPopUpToDestroy()
+    {
+        PopUpData TextBoxData = new PopUpData()
+        {
+            BodyText = "Please Destroy me",
+            BackgroundImage = PopUpImage,
+            width = 400,
+            height = 250,
+            textColor = Color.black,
+            lifeTime = 300f,
+            popUpId = 12,
+        };
+
+        ArrowPopUpData ArrowData = new ArrowPopUpData()
+        {
+            radius = 60,
+            angle = 30,
+            lifeTime = 300f,
+            popUpId = 43,
+        };
+
+        ArrowPopUpData ArrowDataTwo = new ArrowPopUpData()
+        {
+            radius = 60,
+            angle = 170,
+            lifeTime = 300f,
+            popUpId = 4,
+        };
+
+        TutorialServiceLocator.TutorialEventManager.RaiseEvent(TutorialEvent.TextBoxTutorial, position, TextBoxData);
+
+        TutorialServiceLocator.TutorialEventManager.RaiseEvent(TutorialEvent.ArrowTutorial, position, ArrowData);
+
+        TutorialServiceLocator.TutorialEventManager.RaiseEvent(TutorialEvent.ArrowTutorial, position, ArrowDataTwo);
+
+        var popUp = FindObjectsOfType<PopUp>();
+
+        yield return new WaitUntil(() => popUp == null);
+
+    }
+
+    public void CloseAllPopUps()
+    {
+        popUpManager.CloseAllPopUps();
+    }
+
+    public void CloseSinglePopUp()
+    {
+        popUpManager.closePopUp(4);
     }
 
 }
